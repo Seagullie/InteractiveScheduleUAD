bd1a07d2e1c92c0ea93ac5b8c56a1e935fea36ae
Seagullie <59409233+Seagullie@users.noreply.github.com>
2023-06-02 18:55:21 +0300

    Delete template file for Keys.ts

--- constants/Keys.template.ts
+++
@@ -1,4 +0,0 @@
-// this is just a template. Specify your actual keys in Keys.ts (create it if the file doesn't exist)
-
-// export const ContentfulSpace = "your_contentful_space_id_here"
-// export const ContentfulContentDeliveryAccessToken = "your_contentful_access_token_here"
