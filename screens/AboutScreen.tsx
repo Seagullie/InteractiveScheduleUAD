b08663bf3f60d27c35c181db2dabf3621cde3b2f
Seagullie <59409233+Seagullie@users.noreply.github.com>
2023-08-11 17:24:10 +0300

    Bump app version

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -8,7 +8,7 @@ import { useNavigation } from "@react-navigation/native"
 import { DrawerRoutes } from "../routes/DrawerRoutes"
 
 export default function AboutScreen() {
-  const appVersion = "1.0.1"
+  const appVersion = "1.0.2"
   const navigation = useNavigation()
 
   return (
