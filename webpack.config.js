aeded069a15b8502cc6476a60944ad4d6d848681
Seagullie <59409233+Seagullie@users.noreply.github.com>
2024-08-31 15:57:24 +0300

    Add vscode tasks for default terminals; downgrade react-native-web

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv)
   // Customize the config before returning it.
   // config.resolve.alias["../Utilities/Platform"] = "react-native-web/dist/exports/Platform"
-  // config.resolve.alias["react-native$"] = "react-native-web"
+  config.resolve.alias["react-native$"] = "react-native-web"
   // // config.resolve.extensions = [".tsx", ".ts", ".js", ".ios.js", ".android.js", ".web.js"]
 
   return config
